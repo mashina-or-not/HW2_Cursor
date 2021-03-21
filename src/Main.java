710b9091b44d14d0acf90a114fd0acab2ab4eb99
mashina-or-not <slobodaforB@gmail.com>
2021-03-21 12:05:23 +0200

    HW1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,7 +14,7 @@ public class Main {
         int[] positive_negative = {2, 5, -1, 3, -7, -5, 12, 4, -6};
         int resultSumPositive = 0;
 
-        for (Integer number: positive_negative){
+        for (Integer number : positive_negative) {
 
             if (number > 0)
                 resultSumPositive += number;
@@ -29,7 +28,7 @@ public class Main {
         int[] arrayAverageNum = {2, 4, 5, 1, 6, 3, 7, 9, 7};
         double averageNum = 0;
 
-        for (Integer numbers: arrayAverageNum)
+        for (Integer numbers : arrayAverageNum)
             averageNum += numbers;
 
         averageNum /= arrayAverageNum.length;
@@ -37,7 +36,7 @@ public class Main {
 
 
         //Replace all duplicated values by 0
-        Integer[] duplicateArray = {3,2,3,1,4,2,8,3};
+        Integer[] duplicateArray = {3, 2, 3, 1, 4, 2, 8, 3};
 
         for (int i = 0; i < duplicateArray.length; i++) {
 
